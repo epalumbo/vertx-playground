483585913dff597b28089dddf42668962f8c1109
ehpalumbo <ehpalumbo@gmail.com>
2019-06-20 19:27:49 +0200

    Update RxJava example.

--- src/main/java/com/waes/demo/RxJavaPlayground.java
+++ src/main/java/com/waes/demo/RxJavaPlayground.java
@@ -2,6 +2,8 @@ package com.waes.demo;
 
 import io.reactivex.Observable;
 
+import static java.lang.System.err;
+import static java.lang.System.out;
 import static java.lang.Thread.currentThread;
 
 public class RxJavaPlayground {
@@ -10,17 +12,29 @@ public class RxJavaPlayground {
         final Observable<Integer> observable = Observable.create(emitter -> {
             emitter.onNext(1);
             emitter.onNext(3);
+            // emitter.onError(new Exception("ups!"));
             emitter.onComplete();
         });
         observable
-            .doOnNext(i -> System.out.println("first " + i + " - " + currentThread().getName()))
+            .doOnNext(i -> print("first", i))
             .map(i -> i * 2)
-            .doOnNext(i -> System.out.println("second " + i + " - " + currentThread().getName()))
-            //.observeOn(Schedulers.computation())
-            .doOnNext(i -> System.out.println("third " + i + " - " + currentThread().getName()))
-            //.subscribeOn(Schedulers.single())
-            .subscribe(i -> System.out.println("forth " + i + " - " + currentThread().getName()));
+            .doOnNext(i -> print("second", i))
+//            .observeOn(Schedulers.computation())
+//            .delay(100, TimeUnit.MILLISECONDS)
+            .doOnNext(i -> print("third", i))
+//            .subscribeOn(Schedulers.single())
+            .subscribe(
+                i -> print("forth", i),
+                failure -> err.format("error: %s on %s", failure.getMessage(), currentThread().getName()).println(),
+                () -> out.format("completed on %s", currentThread().getName()).println());
+
+        observable.subscribe(out::println);
+
         Thread.sleep(1000);
+    }
+
+    private static void print(String checkpoint, Integer i) {
+        out.format(checkpoint + " %s on %s", i, currentThread().getName()).println();
     }
 
 }
